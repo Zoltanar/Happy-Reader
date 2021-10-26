ed4538acd524742642ba7671ddd4bb51df25c2b1
Zoltanar <universegusty@gmail.com>
2021-10-26 19:53:35 +0100

    Fix: Boolean inversion for TextThread.IsSystem.

---
+++ Happy Reader/View/Converters/BooleanInverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Happy_Reader.View.Converters
+{
+	class BooleanInverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value is not bool bValue) throw new NotSupportedException();
+			return !bValue;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if(targetType != typeof(bool)) throw new NotSupportedException();
+			if (value is bool bValue) return !bValue;
+			if (value is string sValue) return !bool.Parse(sValue);
+			return !System.Convert.ToBoolean(value);
+		}
+	}
+}
