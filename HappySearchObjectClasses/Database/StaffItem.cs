c10f5f5c3b9855f42af108a494eba4e47bac363c
Zoltanar <universegusty@gmail.com>
2023-12-06 16:56:13 +0000

    DatabaseDumpReader: Fix to remove aliasId from staff item.

--- HappySearchObjectClasses/Database/StaffItem.cs
+++ HappySearchObjectClasses/Database/StaffItem.cs
@@ -8,7 +8,6 @@ namespace Happy_Apps_Core.Database;
 public class StaffItem : DumpItem, IDataItem<int>
 {
     public int ID { get; set; }
-    public int AliasID { get; set; }
     public string Gender { get; set; }
     public string Language { get; set; }
     public string Description { get; set; }
@@ -19,12 +18,11 @@ public class StaffItem : DumpItem, IDataItem<int>
     public DbCommand UpsertCommand(DbConnection connection, bool insertOnly)
     {
         string sql = $"INSERT {(insertOnly ? string.Empty : "OR REPLACE ")}INTO {nameof(StaffItem)}s " +
-                     "(ID,AID,Gender,Lang,Desc) VALUES " +
-                     "(@ID,@AID,@Gender,@Lang,@Desc)";
+                     "(ID,Gender,Lang,Desc) VALUES " +
+                     "(@ID,@Gender,@Lang,@Desc)";
         var command = connection.CreateCommand();
         command.CommandText = sql;
         command.AddParameter("@ID", ID);
-        command.AddParameter("@AID", AliasID);
         command.AddParameter("@Gender", Gender);
         command.AddParameter("@Lang", Language);
         command.AddParameter("@Desc", Description);
@@ -36,7 +34,6 @@ public class StaffItem : DumpItem, IDataItem<int>
         try
         {
             ID = Convert.ToInt32(reader["ID"]);
-            AliasID = Convert.ToInt32(reader["AID"]);
             Gender = Convert.ToString(reader["Gender"]);
             Language = Convert.ToString(reader["Lang"]);
             Description = Convert.ToString(reader["Desc"]);
@@ -51,7 +48,6 @@ public class StaffItem : DumpItem, IDataItem<int>
     public override void LoadFromStringParts(string[] parts)
     {
         ID = GetInteger(parts, "id", 1);
-        AliasID = GetInteger(parts, "aid");
         Gender = GetPart(parts, "gender");
         Language = GetPart(parts, "lang");
         Description = GetPart(parts, "description");
